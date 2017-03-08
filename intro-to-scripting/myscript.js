e47af940027ce16a42f3fa82e359f0769f0db245
Richard Pringle <rpring9@gmail.com>
2017-03-07 19:35:58 -0500

    Intro to programming end of lesson.

--- intro-to-scripting/myscript.js
+++ intro-to-scripting/myscript.js
@@ -1,4 +1,21 @@
+
 function main() {
-  var input = document.getElementById("input").value;
-  console.log(input);
+  var x = document.getElementById('input').value;
+
+  var y = add(10, x);
+
+  console.log(y)
+};
+
+function add(a, b) {
+  a = Number(a);
+  b = Number(b);
+  return (a + b);
+} 
+
+function checkPassword(x, y) {
+  // this will only return true is x is equal to y
+  console.log('x = ' + x);
+  console.log('y = ' + y);
+  return (x === y);
 };
